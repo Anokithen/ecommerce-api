e59eea6fc7ffc88fe391a4a35de8c39eaa553a09
Anokithen <anokithen@gmail.com>
2026-06-27 11:13:18 +0530

    every error fixed

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from falsk_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 jwt = JWTManager()
-db =SQLAlchemy()
